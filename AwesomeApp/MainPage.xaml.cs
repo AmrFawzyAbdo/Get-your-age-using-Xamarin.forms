0b12e25b72dd969113070e6add02c4b6911878dc
Amr <amrfawz7.0794@gmail.com>
2020-12-11 20:28:38 +0200

    * .gitignore: Get your age! Xamarin.forms
    * AwesomeApp.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AwesomeApp.csproj:
    * AppDelegate.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * Default.png:
    * AwesomeApp.iOS.csproj:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * AwesomeApp.Android.csproj:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * styles.xml:
    * Tabbar.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon80.png:
    * Icon76.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon120.png:
    * Icon167.png:
    * Icon180.png:
    * icon_round.xml:
    * Icon152.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        int count = 0;
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            
+            count++;
+
+
+            ((Button)sender).Text = $"You clicked {count} times.";
+
+            calcMethod();
+            }
+
+        private void calcMethod()
+        {
+            int calc = Convert.ToInt32(year.Text) - Convert.ToInt32(bornDate.Text);
+
+            if (calc >=1 && calc <= 10)
+            {
+                we.Text = $"Too young, your age is: {calc.ToString()}";
+            }
+            else if (calc > 10 && calc <= 20)
+            {
+                we.Text = $"Be ready for challanges {calc.ToString()}'s Man!";
+            }
+            else if (calc > 20 && calc <= 50)
+            {
+                we.Text = $"Time to keep building, your age is: {calc.ToString()} ";
+            }
+            else if (calc > 50)
+            {
+                we.Text = $"Which you a long life, your age is: {calc.ToString()}";
+            }
+            else if (calc < 0)
+            {
+                we.Text = $"If you were porn in ancient ages you will be: {calc.ToString()}";
+            }
+            else if (calc == 0)
+            {
+                we.Text = $"A miracle you are a {calc.ToString()} year";
+            }
+            else {
+                we.Text = "Try to input corect data";
+            }
+            
+        }
+    }
+}
